925fd98d383bb9f8323b7df44948ec192976049c
simpleboss <gichan.lee@threemindev.com>
2019-07-31 18:31:12 +0700

    max_result

--- 5_matti_block.py
+++ 5_matti_block.py
@@ -47,18 +47,39 @@ def print_look():
         i += 1
 
 
-front_list = [1, 1, 1, 1]
-side_list = [0, 1, 1, 0]
+front_list = [2, 0, 3, 1]
+side_list = [1, 1, 2, 3]
 
-i = 0
-while i < len(front_list):
-    j = 0
-    while j < len(side_list):
-        if front_list[i] == 1 and side_list[j] == 1:
-            front_look[i][j] = 1
-            side_look[j][i] = 1
-        j += 1
-    i += 1
 
-print_look()
+# 1st floor calculation
+def max_floor_calculation():
+    i = 0
+    while i < len(front_list):
+        j = 0
+        while j < len(side_list):
+            if front_list[i] >= 1 and side_list[j] >= 1:
+                front_look[i][j] += 1
+                side_look[j][i] += 1
+            j += 1
+        i += 1
 
+    i = 0
+    while i < len(front_list):
+        if front_list[i] >= 1:
+            front_list[i] -= 1
+        if side_list[i] >= 1:
+            side_list[i] -= 1
+        i += 1
+
+
+while 1 in front_list and 1 in side_list:
+    print('==============')
+    max_floor_calculation()
+    print_look()
+    print(front_list, side_list)
+
+max_result = 0
+for i in front_look:
+    for j in i:
+        max_result += j
+print("max_result ="+str(max_result))
